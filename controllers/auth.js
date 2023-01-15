bb5c55a39fd102b97c01fee1e43fa6032553f559
Fadloovich <arabea169@gmail.com>
2023-01-15 18:38:11 +0200

    Added Job Model

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,27 @@
+const User = require('../models/User');
+const { StatusCodes } = require('http-status-codes');
+const bcrypt = require('bcryptjs');
 
 const register = async (req, res) => {
-    res.send('HEllo Reg');
+    const { name, email, password } = req.body;
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const tempUser = {
+        name,
+        email,
+        password: hashedPassword
+    };
+
+    try {
+        const user = await User.create({ ...tempUser });
+        res.status(StatusCodes.CREATED).json({ user });
+    } catch (error) {
+        res.json({ error: error});
+    }
 };
 
 const login = async (req, res) => {
     res.send('Login');
 };
 
-module.exports = { register, login}
+module.exports = { register, login }
---
+++ models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please Provide a usernane'],
+        minLength: 3,
+        maxLength: 50,
+    },
+    email: {
+        type: String,
+        required: [true, 'Please Provide an email address'],
+        match: [
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            'Please provide a valid email'
+        ],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, 'Please Provide a password'],
+        minLength: 6,
+    }
+});
+
+module.exports = mongoose.model('User', userSchema);
